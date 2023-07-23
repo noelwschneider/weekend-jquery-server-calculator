6398ea514e2d7d4b8313e9c6e75477586a971ebe
noelwschneider <noelwschneider@gmail.com>
2023-07-23 00:07:06 -0500

    added get and post function outlines

--- server/server.js
+++ server/server.js
@@ -6,6 +6,25 @@ const port = 5000
 app.use(express.static('server/public'))
 app.use(bodyParser.urlencoded({extended:true}))
 
+// Answer to send back
+let calculationAnswer;
+// Calculator logic
+
+// Get function
+app.get('/getanswer', (req, res) => {
+    console.log('in server-side GET')
+    res.send(calculationAnswer)
+})
+
+// Post function
+app.post('/submitcalculation', (req, res) => {
+    console.log('In server-side POST')
+    let userCalculation = req.body
+    console.log(userCalculation)
+
+    res.sendStatus(201)
+})
+
 app.listen(port, () => {
     console.log('listening on port', port)
 })
